a3b84ceb9536e044a634fdd0317fb07efcf57d2a
Josh Ellman <josh.ellman@gmail.com>
2017-01-02 14:45:58 -0800

    nav bar and styling with bootstrap and bootswatch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
-  root "posts#index"
+  root 'static_pages#home'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+
+  # resources :posts
+  # root 'posts#index'
 end
